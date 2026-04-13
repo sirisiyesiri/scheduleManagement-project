29ce043c20e8935b04f20694e04e5102a54163b7
sirisiyesiri <shil690321@naver.com>
2026-04-13 14:30:38 +0900

    feat: ScheduleController 생성

---
+++ src/main/java/com/example/schedulemanagement/controller/ScheduleController.java
@@ -0,0 +1,12 @@
+package com.example.schedulemanagement.controller;
+
+import com.example.schedulemanagement.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+}
